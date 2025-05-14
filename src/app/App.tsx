88291391bbbc993d0ec1bf74c7d166efec894a4b
cnandza <cnandza@saudigitus.org>
2025-05-14 12:33:28 +0200

    feat: update lib version

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,11 @@
 import React from 'react'
 import './App.module.css'
 import { Router } from '../components/routes'
-import { AppWrapper } from 'dhis2-semis-components'
-import { HashRouter } from 'react-router-dom'
 
 const MyApp = () => {
 
     return (
-        <AppWrapper dataStoreKey='dataStore/semis/values'>
-            <HashRouter>
-                <Router />
-            </HashRouter>
-        </AppWrapper>
+        <Router />
     )
 }
 
