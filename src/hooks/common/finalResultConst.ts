83923c5aac96bf7dd174023f2faa1f56a6129fc5
cnandza <cnandza@saudigitus.org>
2025-09-15 10:19:44 +0200

    fix(event status): display the correct event status based on the key-value data from useTableData

--- src/hooks/common/finalResultConst.ts
+++ src/hooks/common/finalResultConst.ts
@@ -14,7 +14,7 @@ export const useFinalResultConst = ({ updateData, data }: { data: any[], updateD
                     ?.find(x => x.id === stage)?.programStageDataElements
                     ?.find(x => x.dataElement.id === status)
                     ?.dataElement.optionSet.options
-                    ?.find(x => x.value === copy[index]?.frEvent?.dataValues?.filter((dataValue: any) => dataValue?.dataElement == status)?.[0]?.value)?.label
+                    ?.find(x => x.value === copy[index][status!])?.label
 
 
                 copy[index][status!] = option ? statusComponent({ status: option! }) : undefined
