041a2915b58bed74e04583de5c5ce57d02c63b70
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2025-10-31 16:41:34 +0200

    fix(App): uncomment translation logic for internationalization

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,11 +5,11 @@ import { AppWrapper } from 'dhis2-semis-components'
 import { useConfig } from '@dhis2/app-runtime'
 import { HashRouter } from 'react-router-dom'
 import { D2I18n } from 'dhis2-semis-types'
-import translator from '../locales/index';
+// import translator from '../locales/index';
 
 const MyApp = ({ i18n }: { i18n: D2I18n }) => {
     const { baseUrl } = useConfig()
-    const translation: any = i18n ? i18n : translator
+    // const translation: any = i18n ? i18n : translator
 
     return (
         <AppWrapper
@@ -18,7 +18,7 @@ const MyApp = ({ i18n }: { i18n: D2I18n }) => {
             schoolCalendarKey='dataStore/semis/schoolCalendar'
         >
             <HashRouter>
-                <Router i18n={translation} />
+                <Router i18n={i18n as unknown as any} />
             </HashRouter >
         </AppWrapper>
     )
--- src/components/performPromotion/performPromotion.tsx
+++ src/components/performPromotion/performPromotion.tsx
@@ -122,7 +122,7 @@ export default function PerformPromotion({ selected, setStats, openStats, formDa
         if (!hasValidConfiguration) {
             return i18n.t(`Configuration error. ValidStatusValue not set in DataStore for {{sectionType}}`,
                 {
-                    sectionType: i18n.t(sectionType)
+                    sectionType: i18n.t(sectionType!)
                 }
             );
         }
